3531ba04c0fbf58529c93caf39c4bf8f02eb7e58
Abhimanyu Bose <boseabhimanyu@gmail.com>
2026-09-24 21:09:45 +0530

    Access token is generated automatically if valid refresh token is present

--- backend/app/router/router.go
+++ backend/app/router/router.go
@@ -6,7 +6,9 @@ import (
 	"basic-app/handler"
 	mongorepo "basic-app/repository/mongo"
 	"basic-app/services"
+	"context"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	mongo "go.mongodb.org/mongo-driver/v2/mongo"
@@ -20,6 +22,29 @@ func NewRouter(database *mongo.Database, cfg config.Config) *gin.Engine {
 	// Dependencies
 	userRepository := mongorepo.NewUserRepository(database)
 	authService := services.NewAuthService(userRepository, cfg)
+	refreshToken := func(
+		ctx context.Context,
+		refreshToken string,
+	) (string, string, time.Time, error) {
+		result, err := authService.RefreshToken(ctx, refreshToken)
+		if err != nil {
+			return "", "", time.Time{}, err
+		}
+
+		return result.AccessToken,
+			result.RefreshToken,
+			result.RefreshExpiry,
+			nil
+	}
+
+	authMiddleware := auth.AuthMiddleware(
+		cfg.JWTSecret,
+		cfg.AuthAccessCookie,
+		cfg.AuthRefreshCookie,
+		cfg.CookieSecure,
+		cfg.JWTExpiryHours,
+		refreshToken,
+	)
 	userService := services.NewUserService(userRepository)
 	authHandler := handler.NewAuthHandler(authService, cfg)
 	userHandler := handler.NewUserHandler(userService)
@@ -38,16 +63,14 @@ func NewRouter(database *mongo.Database, cfg config.Config) *gin.Engine {
 		// Public
 		authRoutes.POST("/register", authHandler.Register)
 		authRoutes.POST("/login", authHandler.Login)
+		authRoutes.POST("/refresh", authHandler.Refresh)
 
 		// Authenticated
 		protected := authRoutes.Group("")
-		protected.Use(auth.AuthMiddleware(
-			cfg.JWTSecret,
-			cfg.AuthAccessCookie,
-		))
+		protected.Use(authMiddleware)
 
 		protected.PATCH("/password", authHandler.ChangePassword)
-		protected.POST("/refresh", authHandler.Refresh)
+		//	protected.POST("/refresh", authHandler.Refresh)
 		protected.POST("/logout", authHandler.Logout)
 		protected.PATCH("/me", userHandler.UpdateProfile)
 		protected.PATCH("/me/image", userHandler.UpdateProfilePic)
@@ -57,10 +80,7 @@ func NewRouter(database *mongo.Database, cfg config.Config) *gin.Engine {
 	customerRoutes := r.Group("/api/v1/customers")
 
 	customerRoutes.Use(
-		auth.AuthMiddleware(
-			cfg.JWTSecret,
-			cfg.AuthAccessCookie,
-		),
+		authMiddleware,
 		auth.RequireRoles("admin"),
 	)
 
